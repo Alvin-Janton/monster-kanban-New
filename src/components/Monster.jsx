21072b35ce2c301427e0aff488410cf3419487eb
Alvin <alvinjanton575@gmail.com>
2025-12-06 14:41:05 -0500

    Updated kanban project

--- src/components/Monster.jsx
+++ src/components/Monster.jsx
@@ -39,10 +39,21 @@ export default function Monster({ color, height = "h-24" }) {
   };
 
   return (
+    <div className="relative flex justify-center" style={{ width: "5rem" }}>
+
+      {/* Horns BEHIND the monster */}
+  <img
+    src="horns.png"
+    alt="horns"
+    className="absolute -top-8 scale-200 right-1.5 select-none z-0 pointer-events-none"
+  />
+   
     <div
-      className={`${color} ${height} relative rounded-t-full rounded-b-md flex justify-center items-start pt-2 mb-4`}
+      className={`${color} ${height} relative rounded-t-full rounded-b-md flex justify-center items-start pt-2 mb-4 z-10`}
       style={{ width: "5rem" }}
     >
+
+
       {/* Eyes */}
       <div className="flex space-x-1 mt-2">
         <div
@@ -64,6 +75,15 @@ export default function Monster({ color, height = "h-24" }) {
           />
         </div>
       </div>
+
+      {/* teeth */}
+      <img
+        src="teeth.png"
+        alt="teeth"
+        className="absolute -top- scale-75 right-0.5 select-none z-0 pointer-events-none"
+      >
+      </img>
+    </div>
     </div>
   );
 }
